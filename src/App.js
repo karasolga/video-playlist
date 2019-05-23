63e01cbaa2f44202e1ed531743aa50a66ee13310
Olga Karaseva <mail.karasevaol@gmail.com>
2019-05-23 14:29:41 +0200

    put ITEMS in constants

--- src/App.js
+++ src/App.js
@@ -3,44 +3,7 @@ import FormRow from "./components/form-row";
 import Playlist from "./components/playlist";
 import Player from "./components/player";
 import "./App.css";
-
-const ITEMS = [
-  {
-    artist: "Google",
-    title: "Mountain",
-    videoUrl: "http://thenewcode.com/assets/videos/mountain.mp4"
-  },
-  {
-    artist: "Google",
-    title: "For Bigger Meltdowns",
-    videoUrl:
-      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4"
-  },
-  {
-    artist: "Garage419",
-    title: "Subaru Outback On Street And Dirt",
-    videoUrl:
-      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4"
-  },
-  {
-    artist: "Blender Foundation",
-    videoUrl:
-      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
-    title: "Elephant Dream"
-  },
-  {
-    artist: "HBO",
-    videoUrl:
-      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
-    title: "For Bigger Blazes"
-  },
-  {
-    artist: "Garage419",
-    videoUrl:
-      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4",
-    title: "We Are Going On Bullrun"
-  }
-];
+import ITEMS from './constants/items'
 
 function App() {
   const [items, setItems] = useState(ITEMS);
---
+++ src/constants/items.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    artist: "Google",
+    title: "Mountain",
+    videoUrl: "http://thenewcode.com/assets/videos/mountain.mp4"
+  },
+  {
+    artist: "Google",
+    title: "For Bigger Meltdowns",
+    videoUrl:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4"
+  },
+  {
+    artist: "Garage419",
+    title: "Subaru Outback On Street And Dirt",
+    videoUrl:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/SubaruOutbackOnStreetAndDirt.mp4"
+  },
+  {
+    artist: "Blender Foundation",
+    videoUrl:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4",
+    title: "Elephant Dream"
+  },
+  {
+    artist: "HBO",
+    videoUrl:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4",
+    title: "For Bigger Blazes"
+  },
+  {
+    artist: "Garage419",
+    videoUrl:
+      "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/WeAreGoingOnBullrun.mp4",
+    title: "We Are Going On Bullrun"
+  }
+];
+
