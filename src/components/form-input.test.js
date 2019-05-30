5b1db0beadafdc32a52811a9ad9b1b30eb7dc5a4
Olga Karaseva <mail.karasevaol@gmail.com>
2019-05-31 00:09:19 +0200

    add snapshot test for form input component

---
+++ src/components/form-input.test.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import FormInput from './form-input'
+
+describe('FormInput', () => {
+  it('renders correctly', () => {
+    expect(FormInput).toMatchSnapshot()
+  })
+})
